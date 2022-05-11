db6614b638af5ed8d732969e107657058c55e999
Rasmus Appelgren <rasmus.appelgren@gmail.com>
2022-05-11 14:34:09 +0200

    Iaf löst lite

--- src/MovieList.js
+++ src/MovieList.js
@@ -1,43 +1,56 @@
-import React, {useState, useRef} from "react";
+import React, {useState} from "react";
 import Movie from "./Movie";
+   /* Funktioner
+    Er webbsida ska ha följande funktioner:
 
+   1.  Användare ska kunna lägga till en film genom att ange en titel och ett betyg.
+   2. Ni ska validera att användaren angett både titel och betyg har angetts innan en film kan sparas. Återkoppling ska ske genom popup-rutor.
+   3. Formuläret återställs efter en film är tillagd
+   4. En film i listan ska representeras av titel (text) och stjärnor (så många stjärn-ikoner som betyget filmen har)
+   5. Användare ska kunna ta bort en film genom att klicka på ikonen med ett kryss
+   6. I denna uppgift ska ni implementera sorterings-funktionerna (sortering enligt alfabetisk ordning, samt efter filmens betyg)
+*/
+    
 export default function MovieList() {
     const [movies, setMovies] = useState([{
         id: 1,
         title: "First movie",
-        grade: 1
+        grade: ""
     }]);
+
+    const [title, setTitle] = useState('');
+    const [grade, setGrade] = useState('');
+    function addItem(){
+        const newId = movies.length > 0 ? movies[movies.length - 1].id + 1 : 1;
+        setMovies([...movies, {
+            id: newId,
+            title: title,
+            grade: grade,
+        }]);
     
-
-    const inputRef = useRef();
-    function addItem(event){
-        if (event.keyCode === 13) {
-            const newId = movies.length > 0 ? movies[movies.length - 1].id + 1 : 1;
-
-            setMovies([...movies, {
-                id: newId,
-                title: inputRef.current.value,
-                grade: inputRef.current.value
-            }]);
-            inputRef.current.value = "";
-        }
+        title.current.value = "";
+        grade.current.value = "";
+    
     }
+    
     function deleteItem(id) {
         setMovies(movies.filter((item) => item.id !== id));
 
     }
+
     return (
         <div>
             <form>
-            <input className="form-control" placeholder="Ange filmtitel här"></input>
-            <select>
+            <input className="form-control" placeholder="Ange filmtitel här" value={title}   name="title" onChange={e => setTitle(e.target.value)}></input>
+            <select value={grade} name="grade" onChange={e => setGrade(e.target.value)}> 
                 <option value="1">1</option>
                 <option value="2">2</option>
                 <option value="3">3</option>
                 <option value="4">4</option>
                 <option value="5">5</option>
             </select>
-            <input type="submit" className="btn btn-success mt-3" value="Spara film" ref={inputRef} onSubmit={addItem}></input>
+            
+            <input type="submit" className="btn btn-success mt-3" value="Spara film" onSubmit={e => addItem()}></input>
             </form>
 
             <ul className="list-group">
--- src/test2.js
+++ src/test2.js
@@ -1,44 +1,52 @@
-import React, {useState} from "react";
+import React, {useState, useRef} from "react";
 import Movie from "./Movie";
+   /* Funktioner
+    Er webbsida ska ha följande funktioner:
 
-export default function MovieList() {
-    const [title, setMovies] = useState([{
-        id: 1,
-        title: "First movie",
-    }]);
-    const [grade, setGrade] = useState(1)
+   1.  Användare ska kunna lägga till en film genom att ange en titel och ett betyg.
+   2. Ni ska validera att användaren angett både titel och betyg har angetts innan en film kan sparas. Återkoppling ska ske genom popup-rutor.
+   3. Formuläret återställs efter en film är tillagd
+   4. En film i listan ska representeras av titel (text) och stjärnor (så många stjärn-ikoner som betyget filmen har)
+   5. Användare ska kunna ta bort en film genom att klicka på ikonen med ett kryss
+   6. I denna uppgift ska ni implementera sorterings-funktionerna (sortering enligt alfabetisk ordning, samt efter filmens betyg)
+*/
+function Form() {
+    const [state, setState] = React.useState({
+        id = 1,
+        title: "",
+        grade: ""
+    })
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        const movie = {title, grade}
-
-        console.log(movie)
-
+    function addItem(e) {
+        const newId = movies.length > 0 ? movies[movies.length - 1].id + 1 : 1;
+        setMovies({
+            ...movies, 
+            id: newId,
+            title: inputRef.current.value
+        );
     }
 
-    
-    function deleteItem(id) {
-        setMovies(title.filter((item) => item.id !== id));
 
-    }
     return (
         <div>
-            <form onSubmit={handleSubmit}>
-                <label>Titel:</label>
-                <input className="form-control" placeholder="Ange filmtitel här" value={title} onChange={(e) => setMovies(e.target.value)}></input>
-                <label>Betyg:</label>
-                <select value={grade} onChange={(e) => setGrade(e.target.value)}>
-                    <option value="1">1</option>
-                    <option value="2">2</option>
-                    <option value="3">3</option>
-                    <option value="4">4</option>
-                    <option value="5">5</option>
-                </select>
-                <input type="submit" class="btn btn-success mt-3" value="Spara film"></input>
-            </form>
+            <fieldset>
+            <label>Titel:
+            <input className="form-control" placeholder="Ange filmtitel här" type="text" value={state.title} onChange={handleChange} name="title"></input>
+            </label>
+
+            <select name="grade" onChange={handleChange} value={state.version}>
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+                <option value="5">5</option>
+            </select>
+            <input type="submit" className="btn btn-success mt-3" value="Spara film" onSubmit={addItem}></input>
+            </fieldset>
+
             <ul className="list-group">
-                { title.map(movie => <Movie key={movie.id} item={movie} deleteItem={deleteItem} />)}
+                { state.map(movie => <Movie key={movie.id} item={movie} deleteItem={deleteItem} grade={movie.grade} />)}
             </ul>
-        </div> 
+        </div>
     )
 }
